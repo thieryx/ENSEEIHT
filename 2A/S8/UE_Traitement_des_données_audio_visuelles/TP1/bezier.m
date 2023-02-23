67983d0c1871e67cd6383bd194668c01fc76fe4e
Thierry <thierry.xu@outlook.fr>
2023-02-23 16:18:27 +0100

    ajout nouveaux ue

---
+++ 2A/S8/UE_Traitement_des_données_audio_visuelles/TP1/bezier.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = bezier(inputArg1,inputArg2)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
---
+++ 2A/S8/UE_Traitement_des_données_audio_visuelles/TP1/moindres_carres.m
@@ -0,0 +1,12 @@
+function inconnu = moindres_carres(d, x, mat)
+        [rows,~] = size(mat);
+        A = zeros(rows,d);
+        for i = 1:d
+            A(:,i) = nchoosek(d,i)*(x.^i)*((1-x).^(d-i));
+        end
+        B = mat - mat(1)*((1-x).^d);
+
+        inconnu = A\B;
+
+end
+
